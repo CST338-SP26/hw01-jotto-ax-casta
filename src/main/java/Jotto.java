06e327d0523b88e9a1063d7c6b12fffde0c71e09
ax-casta <xandercasta07@yahoo.com>
2026-02-12 19:25:48 -0800

    Added play method with outline of input options

--- src/main/java/Jotto.java
+++ src/main/java/Jotto.java
@@ -70,6 +70,8 @@ public class Jotto {
     public boolean isDEBUG() {
         return DEBUG;
     }
+
+    //METHOD READWORDS
     public ArrayList<String> readWords(){
         wordList.clear();
         //opens and scans file
@@ -94,8 +96,56 @@ public class Jotto {
         }
         return wordList;
     }
-    public void play(){
 
+    //METHOD PLAY
+    public void play(){
+        //scanner to read user input
+        Scanner scan = new Scanner(System.in);
+
+        //prints menu
+        boolean gameRunning = true;
+        while(gameRunning){
+            System.out.println("=-=-=-=-=-=-=-=-=-=-=");
+            System.out.println("Choose one of the following:");
+            System.out.println("1:\t Start the game");
+            System.out.println("2:\t See the word list");
+            System.out.println("3:\t See the chosen words");
+            System.out.println("4:\t Show Player guesses");
+            System.out.println("zz to exit");
+            System.out.println("=-=-=-=-=-=-=-=-=-=-=");
+            System.out.print("What is your choice: ");
+
+            //input is trimmed
+            String choice = scan.nextLine().trim().toLowerCase();
+            //option 1 : pickword()->guess()
+            if(choice.equals("1")||choice.equals("one")){
+
+            }
+            //option 2 : showWordList()
+            else if(choice.equals("2")||choice.equals("two")){
+
+            }
+            //option 3 : showPlayedWords()
+            else if(choice.equals("3")||choice.equals("three")){
+
+            }
+            //option 4 : showPlayerGuesses()
+            else if(choice.equals("4")||choice.equals("four")){
+
+            }
+            //quits game
+            else if(choice.equals("zz")){
+                //stops the while loop
+                gameRunning = false;
+            }
+            //unknown input
+            else{
+                System.out.println("I don't know what \"" + choice + "\" is.");
+            }
+            //pauses while loop for player input
+            System.out.println("Press enter to continue");
+            scan.nextLine();
+        }
     }
     public int guess() {
         return 0;
